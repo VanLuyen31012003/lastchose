164b2de4e3e191f07fd00461fa95f42dc1de10a7
Van Luyen <vuongluyen2003@gmai.com>
2023-10-28 13:10:03 +0700

    Add project files.

---
+++ lastchose/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using lastchose.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lastchose.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        WebsiteBanHangEntities1 objWebsiteBanHangEntities1 = new WebsiteBanHangEntities1();
+        public ActionResult Index()
+        {
+            var listCategory = objWebsiteBanHangEntities1.Categories.ToList();
+            return View(listCategory);
+        }
+        public ActionResult Productcategory(int Id)
+		{
+            var listProduct = objWebsiteBanHangEntities1.Products.Where(n => n.Categoryld == Id).ToList();
+            return View(listProduct);
+		}
+    }
+}
---
+++ lastchose/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using lastchose.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lastchose.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        WebsiteBanHangEntities1 objWebsiteBanHangEntities1 = new WebsiteBanHangEntities1();
+        public ActionResult Detail(int Id)
+        {
+            var objProduct = objWebsiteBanHangEntities1.Products.Where(n=>n.Id==Id).FirstOrDefault();
+            return View(objProduct);
+        }
+    }
+}
